8277fd98894013d097018413a7b890b58f663b37
Brendan Falk <4949076+brendanfalk@users.noreply.github.com>
2025-06-18 14:52:07 -0700

    default is dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
